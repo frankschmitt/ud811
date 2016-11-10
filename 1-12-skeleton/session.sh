be896dc4a9f20a01698894d5ffcab8f4eabcaa9a
Frank Schmitt <frank@qwhon.de>
2016-11-10 20:29:56 +0100

    use cached response on initial load

--- 1-12-skeleton/session.sh
+++ 1-12-skeleton/session.sh
@@ -1,7 +1,7 @@
 # vim session with source files
 tmux "new-session" -s "uda801" -d
 tmux send-keys -t uda801 'cd /Users/frank/src/udacity-progressive-web-apps-ud811/1-12-skeleton' C-m 
-tmux send-keys -t uda801 'vim public/*.js public/*.html README.md' C-m
+tmux send-keys -t uda801 'vim public/scripts/*.js public/*.html README.md' C-m
 # run HTTP server
 tmux "split-window" -v -p 30
 tmux send-keys -t uda801 'cd /Users/frank/src/udacity-progressive-web-apps-ud811/1-12-skeleton' C-m 
